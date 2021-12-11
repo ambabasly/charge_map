22c5ae33d82112817f6aacce799eaebfc85cb625
Sylvester Arikhan <arikhansylvester@gmail.com>
2021-12-11 21:10:08 +0100

    First commit

---
+++ src/components/locationmap/locationmap.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import {
+  EnvironmentFilled
+} from '@ant-design/icons';
+// import { Modal } from 'antd'
+
+class SimpleMap extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false,
+      current: null
+    }
+  }
+
+  // default props
+  static defaultProps = {
+    zoom: 10
+  };
+
+  // charge center maker
+  ChargeCenter = (location) => {
+    return (
+      <div
+        onClick={() => {
+          this.open(location)
+        }}
+        lat={location.AddressInfo.Latitude}
+        lng={location.AddressInfo.Longitude}
+      >
+        <EnvironmentFilled
+          style={{
+            fontSize: '40px'
+          }}
+        />
+      </div>
+    )
+  }
+
+  open = (current) => {
+    console.log(current)
+    this.setState({
+      current,
+      visible: true
+    })
+  }
+
+  close = () => {
+    this.setState({
+      current: null,
+      visible: false
+    })
+  }
+
+
+  render() {
+    const { locations } = this.props
+    // const { visible, current } = this.state
+
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100%', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyD-D9FLKf6qTzH_ke7fOpodZjKfSg_e87g' }}
+          defaultCenter={{
+            lat: this.props.lat,
+            lng: this.props.long
+          }}
+          defaultZoom={this.props.zoom}
+          
+        >
+          {/* <Home
+            lat={this.props.lat}
+            lng={this.props.long}
+          /> */}
+
+          {
+            locations.map((item) => (
+              this.ChargeCenter(item)
+            ))
+          }
+        </GoogleMapReact>
+
+        {/* {
+          current !== null &&
+          <Modal
+            footer={null}
+            title={null}
+            onCancel={this.close}
+            onOk={this.close}
+            visible={true}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis soluta, itaque repellat iste molestiae iure fugit neque reiciendis optio esse molestias possimus minima sequi ullam earum explicabo rem saepe iusto.
+          </Modal>
+        } */}
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,56 @@
+import React, { useMemo, useEffect, useCallback, useState } from 'react'
+import axios from 'axios'
+import SimpleMap from '../../components/locationmap/locationmap'
+import { message } from 'antd'
+
+const Homepage = () => {
+  const [locations, setLocations] = useState([])
+
+  // request details
+  const request = useMemo(() => {
+    return {
+      key: 'dbd3949c-d7ea-41aa-81d1-e5d6489e1b',
+      countrycode: 'DE'
+    }
+  }, [])
+
+  const getPoi = useCallback(
+    async () => {
+      try {
+        const res = await axios.get(`https://api.openchargemap.io/v3/poi`, {
+          params: {
+            ...request
+          }
+        })
+        setLocations(res.data)
+      } catch (error) {
+        message.error('Something went wrong')
+      }
+    },
+    [request],
+  )
+
+  useEffect(() => {
+    getPoi()
+  }, [getPoi])
+
+  return (
+    <div className="homepage">
+      <h1 className="title">
+        Charge Point Finder
+      </h1>
+      <div className="map-holder">
+
+        <SimpleMap
+          // long and lat for germany
+          lat={51.1657}
+          long={10.4515}
+          // loations gotten from the endpoint
+          locations={locations}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
